54e1aa94c7c6e19612212d55505026fc6d783134
nomad0 <f16123@gmail.com>
2014-11-01 17:11:27 +0200

    5

--- angular/app.js
+++ angular/app.js
@@ -1,8 +1,8 @@
 var todomvc = angular.module('todomvc', []);
 
-todomvc.controller('todoCtrl', function($scope){
+todomvc.controller('todoCtrl', function($scope, todoStorage){
 
-$scope.todosList = [];
+$scope.todosList = todoStorage.get();;
 $scope.todoFilter=null;
 
 $scope.addTodo=function(){
@@ -15,7 +15,9 @@ text:$scope.newTodo,
 completed: false
 });
 $scope.newTodo = '';
+todoStorage.put($scope.todosList);
 };
+
 
 $scope.startEdit = function (todo) {
 text = todo.text;
@@ -24,14 +26,17 @@ $scope.editedTodo = todo;
 
 $scope.doneEditing = function () {
 $scope.editedTodo = null;
+todoStorage.put($scope.todosList);
 };
 
 $scope.removeTodo = function (todo) {
 $scope.todosList.splice($scope.todosList.indexOf(todo), 1);
+todoStorage.put($scope.todosList);
 };
 
 $scope.completeTodo = function (todo){
 todo.completed = !todo.completed;
+todoStorage.put($scope.todosList);
 };
 
 $scope.selectFilter = function(arg) {
@@ -41,6 +46,7 @@ $scope.todoFilter=arg;
 $scope.checkAll = function() {
 $scope.todosList.forEach(function(todo){
 todo.completed=$scope.masterCheckbox;
+todoStorage.put($scope.todosList);
 });
 };
 
@@ -48,6 +54,7 @@ $scope.clearCompleted = function() {
 $scope.todosList.forEach(function(todo){
 if(todo.completed==true){
 $scope.removeTodo(todo);}
+todoStorage.put($scope.todosList);
 });
 };
 
